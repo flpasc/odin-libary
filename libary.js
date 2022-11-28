5dcd74d21e62feb64397f7b375bd016267dd5ce3
Florian <florianpaulschoene@gmail.com>
2022-11-28 20:28:27 +0100

    some js

--- libary.js
+++ libary.js
@@ -1,34 +1,13 @@
-let myLibary = [];
+let newBook = document.getElementById("new-book");
+let addBook = document.getElementById("submit");
 
-class Book {
-	constructor(title, author, pages, read) {
-		this.title = title;
-		this.author = author;
-		this.pages = pages;
-		this.read = read;
-
-		this.info = function () {
-			return `${title} by ${author}, ${pages} pages, ${read}`;
-		};
-	}
+function openForm() {
+	document.getElementById("add-book").style.display = "block";
 }
 
-let book1 = new Book("Fantasy", "Wizard Oz", "1287", "not read");
-let book2 = new Book("History", "Old Man", "8325257", "read");
-let book3 = new Book("Thriller", "Scare Man", "233", "read");
-let book4 = new Book("Sci-fi", "R2D2", "127", "not read");
-
-function showLibary() {
-	myLibary.forEach((book) => console.table(book));
+function closeForm() {
+	document.getElementById("add-book").style.display = "none";
 }
 
-function addToLibary(newBook) {
-	myLibary.push(newBook);
-}
-
-addToLibary(book1);
-addToLibary(book2);
-addToLibary(book3);
-addToLibary(book4);
-
-showLibary();
+newBook.addEventListener("click", openForm);
+addBook.addEventListener("submit", closeForm);
